3f0f1a35ed4d576205bceb17ea9b1e940f16f988
Eric Quanz <eric.quanz@gmail.com>
2018-04-13 15:53:04 -0300

    Improve test coverage

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
 import React from "react";
 import App from "./App";
 
-import renderer from "react-test-renderer";
+import { create } from "react-test-renderer";
 
-it("renders without crashing", () => {
-  const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toBeTruthy();
+const app = create(<App />);
+
+it("renders without crashing", () => expect(app.toJSON()).toMatchSnapshot());
+
+it("behaves as expected", () => {
+  const expected = {
+    a: 125,
+    b: 100,
+    c: 100,
+    result: 80
+  };
+  const instance = app.getInstance();
+
+  // TODO test event handlers instead?
+  instance.handleChangeText("a", 125);
+  instance.handleChangeText("b", 100);
+  instance.handleChangeText("c", 100);
+
+  expect(instance.state).toMatchObject(expected);
 });
