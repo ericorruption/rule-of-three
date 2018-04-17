eb9a16f4cafdaadc52c6a03bf004b811ffa44f4c
Eric Quanz <eric.quanz@gmail.com>
2018-04-17 15:17:11 -0300

    Improve menubar scripts

--- App.menubar.js
+++
@@ -1,7 +0,0 @@
-const menubar = require("menubar");
-menubar({
-  dir: "dist",
-  width: 280,
-  height: 180,
-  windowPosition: "trayRight"
-});
---
+++ assets/menubar/index.js
@@ -0,0 +1,7 @@
+const menubar = require('menubar');
+
+menubar({
+  width: 280,
+  height: 180,
+  windowPosition: 'trayRight'
+});
