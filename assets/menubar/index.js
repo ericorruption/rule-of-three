fa739ea026f8d91ae5519c87e4edbd11cfac9180
Eric Quanz <eric.quanz@gmail.com>
2021-06-29 13:09:28 +0200

    deprecate master in favor of main

--- assets/menubar/index.js
+++
@@ -1,7 +0,0 @@
-const menubar = require('menubar');
-
-menubar({
-  width: 280,
-  height: 180,
-  windowPosition: 'trayRight'
-});
--- src/calculate.js
+++
@@ -1,1 +0,0 @@
-export default ({ a = 0, b = 0, c = 0 }) => b * c / a || 0;
